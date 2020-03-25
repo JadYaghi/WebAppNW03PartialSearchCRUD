2b406f4771d950ea8f2359ae4a22351702ca81f5
Robbin Law <robbinl@telusplanet.net>
2020-03-25 14:32:14 -0600

    Add project files.

---
+++ WebApp/Pages/MultiRecordQueryWithDefaultGridView.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="MultiRecordQueryWithDefaultGridView.aspx.cs" Inherits="WebApp.ExercisePages.MultiRecordQueryWithDefaultGridView" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1> Multi Record Query with Default GridView</h1>
+    <div class="offset-2">
+        <asp:DataList ID="Message" runat="server" Enabled="False">
+        <ItemTemplate>
+            <%# Container.DataItem %>
+        </ItemTemplate>
+    </asp:DataList>
+    <br />
+        <asp:Label ID="Label1" runat="server" Text="Select an Item: "></asp:Label>&nbsp;&nbsp
+        <asp:TextBox ID="PartialProductNameV2" runat="server"></asp:TextBox>
+        <asp:Button ID="SearchProductsPartial" runat="server" Text="Search Products"
+            OnClick="SearchProductsPartial_Click" />
+        <br />
+        <br />
+        <asp:Label ID="MessageLabel" runat="server" ></asp:Label>
+        <br />
+        <asp:GridView ID="ProductGridViewV2" runat="server">
+        </asp:GridView>
+
+
+
+
+        
+    </div>
+</asp:Content>
---
+++ WebApp/Pages/MultiRecordQueryWithDefaultGridView.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using DBSystem.BLL;
+using DBSystem.ENTITIES;
+
+namespace WebApp.ExercisePages
+{
+    public partial class MultiRecordQueryWithDefaultGridView : System.Web.UI.Page
+    {
+        List<string> errormsgs = new List<string>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MessageLabel.Text = "";
+            if (!Page.IsPostBack)
+            {
+                //BindList();
+            }
+        }
+
+
+        protected Exception GetInnerException(Exception ex)
+        {
+            //drill down to the inner most exception
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+            }
+            return ex;
+        }
+
+        protected void LoadMessageDisplay(List<string> errormsglist, string cssclass)
+        {
+            Message.CssClass = cssclass;
+            Message.DataSource = errormsglist;
+            Message.DataBind();
+        }
+
+
+        protected void SearchProductsPartial_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(PartialProductNameV2.Text))
+            {
+                errormsgs.Add("Please enter a partial product name for the search");
+                LoadMessageDisplay(errormsgs, "alert alert-info");
+                ProductGridViewV2.DataSource = null;
+                ProductGridViewV2.DataBind();
+            }
+            else
+            {
+                try
+                {
+                    Controller02 sysmgr = new Controller02();
+                    List<Entity02> info = sysmgr.FindByPartialName(PartialProductNameV2.Text);
+                    if (info.Count == 0)
+                    {
+                        errormsgs.Add("No data found for the partial product name search");
+                        LoadMessageDisplay(errormsgs, "alert alert-info");
+                    }
+                    else
+                    {
+                        info.Sort((x, y) => x.ProductName.CompareTo(y.ProductName));
+                        //load the multiple record control
+
+                        //GridView
+                        ProductGridViewV2.DataSource = info;
+                        ProductGridViewV2.DataBind();
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errormsgs.Add(GetInnerException(ex).ToString());
+                    LoadMessageDisplay(errormsgs, "alert alert-danger");
+                }
+            }
+        }
+
+
+
+
+
+        //protected void BindList()
+        //{
+        //    try
+        //    {
+        //        Controller01 sysmgr = new Controller01();
+        //        List<Entity01> info = null;
+        //        info = sysmgr.List();
+        //        info.Sort((x, y) => x.CategoryName.CompareTo(y.CategoryName));
+        //        List01.DataSource = info;
+        //        List01.DataTextField = nameof(Entity01.CategoryName);
+        //        List01.DataValueField = nameof(Entity01.CategoryID);
+        //        List01.DataBind();
+        //        List01.Items.Insert(0, "select...");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageLabel.Text = ex.Message;
+        //    }
+        //}
+        //protected void Fetch_Click(object sender, EventArgs e)
+        //{
+        //    if (List01.SelectedIndex == 0)
+        //    {
+        //        MessageLabel.Text = "Select a category to view its products";
+        //    }
+        //    else
+        //    {
+        //        try
+        //        {
+        //            Controller02 sysmgr = new Controller02();
+        //            List<Entity02> info = null;
+        //            info = sysmgr.FindByID(int.Parse(List01.SelectedValue));
+        //            info.Sort((x, y) => x.ProductName.CompareTo(y.ProductName));
+        //            List02.DataSource = info;
+        //            List02.DataBind();
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            MessageLabel.Text = ex.Message;
+        //        }
+        //    }
+        //}
+    }
+}
